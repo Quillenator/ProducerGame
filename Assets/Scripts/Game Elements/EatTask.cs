4c58d76150e835b49ac606f437e32251f982370d
Quillenator <32397666+Quillenator@users.noreply.github.com>
2017-09-29 22:10:22 -0400

    Stupid stuff
    I made some basic ass shit and its doodoo, gonna make some more basic ass shit and hope its less doodoo

---
+++ Assets/Scripts/Game Elements/EatTask.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EatTask : MonoBehaviour {
+
+    [SerializeField]
+    private bool canEat;
+
+	
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+    void OnCollisionEnter(Collision col)
+    {
+        print("Collided with... " + col.gameObject.name);
+        
+        if(canEat && col.gameObject.name == "Task")
+        {
+            col.gameObject.SetActive(false);
+        }
+    }
+}
